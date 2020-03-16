6e80a7974a3f1a91dd7c6eaa280a1f2f2c5a1746
Luke Moryl <luke.moryl@bird.co>
2020-03-16 09:00:57 -0700

    init

---
+++ internal-python-libraries/me/dea/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='me-dea',
+      version='0.1',
+      description='The funniest joke in the world',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['me.dea'],
+      zip_safe=False)
+
---
+++ internal-python-libraries/me/eda/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='me-eda',
+      version='0.1',
+      description='The funniest joke in the world',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['me.eda'],
+      zip_safe=False)
+
